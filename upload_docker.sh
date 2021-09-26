a5360b9c9e8fcca7292ec94f12fe28d57e1111ac
Andrew Wilson <andero.wil@gmail.com>
2021-09-26 17:32:25 +0000

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=wils93/udacity_ml_microservice_prj4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login -u wils93
+docker tag $1 $dockerpath:latest
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
