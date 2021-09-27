e28bb0493d8b6becf10d679bc6bedf05f20886af
Andrew Wilson <andero.wil@gmail.com>
2021-09-27 08:44:59 +0000

    Run container in k8s cluster

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=wils93/udacity_ml_microservice_prj4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-ml \
+    --image=$dockerpath \
+    --image-pull-policy="Always"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-ml 8000:80
